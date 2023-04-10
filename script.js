4c6afa0af8bcd6b014c893fc3bfa05b4491a1995
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-04-10 12:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const person = {
   age: 80,
 };
 
+const team = players;
+
+const team1 = [];
+players.foreach((item) => {
+	team1.push(item);
+});
+
+const cap1 = {};
+let keys = Object.keys(person);
+keys.foreach((key) => {
+	cap1.key = person.key;
+});
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
